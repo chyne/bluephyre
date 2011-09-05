ee9795fbd83584a07efb0facebac5691af3b6297
Christopher Hyne <christopher.hyne@bluephyre.com>
2011-09-05 14:21:30 -0400

    Added Test project

--- tst/BluePhyreTests/Properties/AssemblyInfo.cs
+++ tst/BluePhyreTests/Properties/AssemblyInfo.cs
@@ -1,14 +1,15 @@
 ﻿using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("BluePhyre")]
+[assembly: AssemblyTitle("BluePhyreTests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("BluePhyre")]
+[assembly: AssemblyProduct("BluePhyreTests")]
 [assembly: AssemblyCopyright("Copyright © Microsoft 2011")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -19,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("c382dfe6-e9a6-48f0-b6ba-ebb1890d3d5c")]
+[assembly: Guid("1d844777-4bf7-436d-a595-efb1924e577c")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -28,7 +29,7 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Revision and Build Numbers 
+// You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
